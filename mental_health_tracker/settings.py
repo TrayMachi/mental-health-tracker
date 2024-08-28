d6324397ab3c48483f6688a70b7e6f955546d654
Tristan A. <tristanagraa@gmail.com>
2024-08-28 15:25:15 +0700

    chore: Add production host to ALLOWED_HOSTS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--@)q0wz!)iy5ndur9wi@j$htb1i6b9_ni-7ctcbt2^q*^8o54&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tristan-agra-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
