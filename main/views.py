7a9ca162452c41286ba992c03741b9fa811a0a4f
Tristan A. <tristanagraa@gmail.com>
2024-10-02 15:45:41 +0700

    feat: finish tutorial 5

--- main/views.py
+++ main/views.py
@@ -9,17 +9,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 
 @login_required(login_url="/login")
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
+    
 
     context = {
         "npm": "2306245112",
         "name": request.user.username,
         "class": "PBP D",
-        "mood_entries": mood_entries,
+        
         "last_login": request.COOKIES["last_login"],
     }
 
@@ -37,6 +40,23 @@ def create_mood_entry(request):
 
     context = {"form": form}
     return render(request, "create_mood_entry.html", context)
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) # strip HTML tags!
+    feelings = strip_tags(request.POST.get("feelings")) # strip HTML tags!
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
 
 def edit_mood(request, id):
     # Get mood entry berdasarkan id
@@ -76,14 +96,14 @@ def show_json(request):
 
 
 def show_xml_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(
         serializers.serialize("xml", data), content_type="application/xml"
     )
 
 
 def show_json_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(
         serializers.serialize("json", data), content_type="application/json"
     )
@@ -110,8 +130,10 @@ def login_user(request):
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie("last_login", str(datetime.datetime.now()))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
 
     else:
         form = AuthenticationForm(request)
